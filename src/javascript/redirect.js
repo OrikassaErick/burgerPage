e355ec608f8fc33be17837f6663fb5bd25f721f4
OrikassaErick <erickorik304@gmail.com>
2025-09-01 20:12:45 -0300

    todos os redirects foram feitos

--- src/javascript/redirect.js
+++ src/javascript/redirect.js
@@ -1,7 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#order-button").addEventListener('click', redirectorderPage)
-
-    function redirectorderPage(){
-        location.href = "pages/orderPage.html"
+    const orderBtn = document.querySelector("#order-button");
+    if (orderBtn) {
+        orderBtn.addEventListener("click", () => {
+            location.href = "pages/orderPage.html";
+        });
     }
-})
+
+    const redirect = [
+        {id: "bacon", url: "../pages/bacon.html"},
+        {id: "batataFrita", url: "../pages/batataFrita.html"},
+        {id: "batatasRusticas", url: "../pages/batatasRusticas.html"},
+        {id: "boloDeCenoura", url: "../pages/boloDeCenoura.html"},
+        {id: "boloDeChocolate", url: "../pages/boloDeChocolate.html"},
+        {id: "boloMousse", url: "../pages/boloMousse.html"},
+        {id: "brocolis", url: "../pages/brocolis.html"},
+        {id: "cafeGelado", url: "../pages/cafeGelado.html"},
+        {id: "cafeLatteQuente", url: "../pages/cafeLatteQuente.html"},
+        {id: "capuccinoTradicional", url: "../pages/capuccinoTradicional.html"},
+        {id: "casquinha", url: "../pages/casquinha.html"},
+        {id: "cheddar", url: "../pages/cheddar.html"},
+        {id: "classicBacon", url: "../pages/classicBacon.html"},
+        {id: "cogumelosFritos", url: "../pages/cogumelosFritos.html"},
+        {id: "donutClassico", url: "../pages/donutClassico.html"},
+        {id: "frangoFrito", url: "../pages/frangoFrito.html"},
+        {id: "guacamole", url: "../pages/guacamole.html"},
+        {id: "jalapeno", url: "../pages/jalapeno.html"},
+        {id: "latteGelado", url: "../pages/latteGelado.html"},
+        {id: "miniHamburgueres", url: "../pages/miniHamburgueres.html"},
+        {id: "miniSalgados", url: "../pages/miniSalgados.html"},
+        {id: "miniTorradas", url: "../pages/miniTorradas.html"},
+        {id: "mojitoClassico", url: "../pages/mojitoClássico.html"},
+        {id: "molhoBarbecue", url: "../pages/molhoBarbecue.html"},
+        {id: "molhoCheddar", url: "../pages/molhoCheddar.html"},
+        {id: "mushroom", url: "../pages/mushroom.html"},
+        {id: "onion", url: "../pages/onion.html"},
+        {id: "onionRings", url: "../pages/onionRings.html"},
+        {id: "palitosDeQueijo", url: "../pages/palitosDeQueijo.html"},
+        {id: "pasteizinhosAssados", url: "../pages/pasteizinhosAssados.html"},
+        {id: "pinaColada", url: "../pages/pinaColada.html"},
+        {id: "queijosVariados", url: "../pages/queijosVariados.html"},
+        {id: "saladaColeslaw", url: "../pages/saladaColeslaw.html"},
+        {id: "sangriaDeFrutas", url: "../pages/sangriaDeFrutas.html"},
+        {id: "smoky", url: "../pages/smoky.html"},
+        {id: "sucoDeLaranja", url: "../pages/sucoDeLaranja.html"},
+        {id: "tartelete", url: "../pages/tartelete.html"},
+        {id: "tortaDeMaca", url: "../pages/tortaDeMaca.html"},
+        {id: "tortaDeMacaClassica", url: "../pages/tortaDeMacaClassica.html"},
+        {id: "vinagreteDeAbacaxi", url: "../pages/vinagreteDeAbacaxi.html"},
+    ] // Vou usar um array de objetos, pois caso seja necessário adicionar mais alguma informação no futuro, seja possível
+
+    for(let i = 0; i < redirect.length; i++){
+
+        const search = document.querySelector(`#${redirect[i].id}`)
+        if(search){
+            search.addEventListener("click", () => {
+                location.href = redirect[i].url
+            })
+        }
+    }
+});
